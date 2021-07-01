003b6d41b9a6e0d02438a31b297d1868aadcc8b8
gutemberg234 <gute.tec@gmail.com>
2021-07-01 06:31:39 -0300

    Primeiro componente React

---
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -0,0 +1,37 @@
+const repositoryName = 'unform2';
+
+export function RepositoryList() {
+    return(
+        <section className="repository-list">
+            <h1>Lista de repositórios</h1>
+
+            <ul>
+                <li>
+                    <strong>{repositoryName}</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+                <li>
+                    <strong>Unform</strong>
+                    <p>Forms in React</p>
+                    <a href="#">Acessar Repositório</a>
+                </li>
+            </ul>
+        </section>
+    )
+}
