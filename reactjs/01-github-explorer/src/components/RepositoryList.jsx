118180018048504373591cb04f70afd9c1342a31
gutemberg234 <gute.tec@gmail.com>
2021-07-02 06:31:42 -0300

    Utilizando o useEffect

--- reactjs/01-github-explorer/src/components/RepositoryList.jsx
+++ reactjs/01-github-explorer/src/components/RepositoryList.jsx
@@ -1,8 +1,9 @@
+import { useEffect, useState } from 'react';
 import { RepositoryItem } from './RepositoryItem';
 
 import '../styles/repositories.scss';
 
-const repositoryName = 'unform2';
+//https://api.github.com/users/Gutemberg234/repos
 
 const biribiri = {
     name: 'Unform',
@@ -10,8 +11,15 @@ const biribiri = {
     link: 'https://www.google.com.br'
 }
 
-
 export function RepositoryList() {
+    const [repos, setRepos] = useState([]);
+
+    useEffect(() => {
+        fetch('https://api.github.com/users/Gutemberg234/repos')
+        .then(response => response.json())
+        .then(data => setRepos(data))
+    }, [])
+    
     return(
         <section className="repository-list">
             <h1>Lista de repositórios</h1>
