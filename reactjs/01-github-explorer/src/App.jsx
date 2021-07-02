c7990915c2bacbf4e6b70702a129ce319ac982f3
gutemberg234 <gute.tec@gmail.com>
2021-07-02 02:43:44 -0300

    Estado do componente

--- reactjs/01-github-explorer/src/App.jsx
+++ reactjs/01-github-explorer/src/App.jsx
@@ -1,7 +1,8 @@
 import { RepositoryList } from './components/RepositoryList';
+import { Counter } from './components/Counter';
 
 import './styles/global.scss'
 
 export function App(){
-    return <RepositoryList />
+    return <Counter />
 }
