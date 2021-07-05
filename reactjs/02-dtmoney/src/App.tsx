ef2963809f13c6fa4b97d9d1e2340648d253da68
gutemberg234 <gute.tec@gmail.com>
2021-07-05 05:05:55 -0300

    Instalando Styled Components

--- reactjs/02-dtmoney/src/App.tsx
+++ reactjs/02-dtmoney/src/App.tsx
@@ -1,7 +1,21 @@
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  color: #8257e6;
+  font-size: 25px;
+  font-family: sans-serif;
+  span{
+    color: red;
+  }
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>
+        Hello World
+        <span>!</span>
+        </Title>
     </div>
   );
 }
