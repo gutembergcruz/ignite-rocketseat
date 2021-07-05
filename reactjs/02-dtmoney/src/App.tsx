eadab95c4299fe29c584ce8d02cbac8cbd507715
gutemberg234 <gute.tec@gmail.com>
2021-07-05 05:15:02 -0300

    Criando estilos globais

--- reactjs/02-dtmoney/src/App.tsx
+++ reactjs/02-dtmoney/src/App.tsx
@@ -1,21 +1,10 @@
-import styled from 'styled-components'
-
-const Title = styled.h1`
-  color: #8257e6;
-  font-size: 25px;
-  font-family: sans-serif;
-  span{
-    color: red;
-  }
-`
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <div className="App">
-      <Title>
-        Hello World
-        <span>!</span>
-        </Title>
+      <h1>Hello World</h1>
+      <GlobalStyle />
     </div>
   );
 }
