20b912a51ffd7974a8443f039eff9e902fd04b37
gutemberg234 <gute.tec@gmail.com>
2021-07-07 11:34:52 -0300

    Estilizando modal

--- reactjs/02-dtmoney/src/styles/global.ts
+++ reactjs/02-dtmoney/src/styles/global.ts
@@ -75,5 +75,17 @@ export const GlobalStyle = createGlobalStyle`
         position: relative;
         border-radius: 0.25rem;
     }
+    .react-modal-close{
+        position: absolute;
+        right: 1.5rem;
+        top: 1.5rem;
+        border: 0;
+        outline: none;
+        background: transparent;
+        transition: filter 0.2s;
 
+        &:hover{
+            filter: brightness(0.8);
+        }
+    }
 `
