f6f52a7e9f1e596c6a401ecdf35035ce1e489528
Gutemberg Cruz <gutemberg@x-apps.com.br>
2021-07-09 03:11:02 -0400

    context api

--- reactjs/02-dtmoney/src/App.tsx
+++ reactjs/02-dtmoney/src/App.tsx
@@ -5,6 +5,7 @@ import { Header } from "./components/Header";
 import { NewTransactionModal } from './components/NewTransactionModal';
 
 import { GlobalStyle } from "./styles/global";
+import { TransactionsContext } from './TransactionsContext';
 
 Modal.setAppElement('#root');
 
@@ -19,11 +20,11 @@ export function App() {
         setIsNewTransactionModalOpen(false)
     }
   return (
-    <>
+    <TransactionsContext.Provider value={[]}>
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal} />
       <Dashboard />
       <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal} />
       <GlobalStyle />
-    </>
+    </TransactionsContext.Provider>
   );
 }
