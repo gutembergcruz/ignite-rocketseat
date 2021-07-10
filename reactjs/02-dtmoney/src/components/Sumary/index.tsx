d250e4c455d08194af041efe293a294cd5676dd8
Gutemberg Cruz <gutemberg@x-apps.com.br>
2021-07-10 04:52:10 -0400

    Criando Hook

--- reactjs/02-dtmoney/src/components/Sumary/index.tsx
+++ reactjs/02-dtmoney/src/components/Sumary/index.tsx
@@ -1,13 +1,12 @@
-import { useContext } from 'react'
 import incomeImg from '../../assets/income.svg'
 import outcomeImg from '../../assets/outcome.svg'
 import totalImg from '../../assets/total.svg'
-import { TransactionsContext } from '../../TransactionsContext'
+import { useTransactions } from '../../hooks/useTransactions'
 
 import { Container } from "./styles"
 
 export function Sumary() {
-    const { transactions } = useContext(TransactionsContext);
+    const { transactions } = useTransactions()
 
     const sumary = transactions.reduce((acc, transactions) => {
 
